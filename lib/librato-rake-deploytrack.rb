2869e412b92f178194ef8227f3974f92ed207e84
Ole Michaelis <Ole.Michaelis@googlemail.com>
2014-01-22 17:21:08 +0100

    also make it load magically without rails

--- lib/librato-rake-deploytrack.rb
+++ lib/librato-rake-deploytrack.rb
@@ -1,9 +1,16 @@
 module Deploytrack
 
-  class DeploytrackRailtie < Rails::Railtie
+  def self.load_rake
     gem_dir = File.dirname __FILE__
-    rake_tasks do
-      Dir.glob("#{gem_dir}/tasks/*.rake").each { |r| load r }
-    end
+    Dir.glob("#{gem_dir}/tasks/*.rake").each { |r| load r }
   end
+
+  if defined?(Rails::Railtie) == 'constant' && Rails::Railtie.class == Class
+    class DeploytrackRailtie < Rails::Railtie
+      rake_tasks { Deploytrack::load_rake }
+    end
+  else
+    load_rake
+  end
+
 end
